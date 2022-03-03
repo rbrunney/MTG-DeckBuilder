84af4d37fe1c1a7cee1c290f2da7f366935252be
rbrunney <rbrunney@student.neumont.edu>
2022-03-02 19:57:41 -0700

    WPF Application Made

---
+++ MTG DeckBuilder/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MTG_DeckBuilder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MTG DeckBuilder/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MTG_DeckBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
